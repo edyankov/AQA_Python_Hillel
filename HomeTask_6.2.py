d45e92f216522b5afd43725c491b275dfb44773e
edyankov <yankov.edik@gmail.com>
2022-12-02 17:33:36 +0200

    Init commit HomeTask_6.2

--- HomeTask_6.2.py
+++ HomeTask_6.2.py
@@ -19,11 +19,11 @@ access_permissions = {
     'R': 'read',
     'X': 'execute'
 }
-for i in range(int(input())):
-    x = input().split()
+for i in range(int(input("Enter amount of files: "))):
+    x = input("Enter name of file: ").split()
     file_permissions[x[0]] = [access_permissions[i] for i in x[1:]]
-for i in range(int(input())):
-    comm, n = input().split()
+for i in range(int(input("Enter amount of actions on files: "))):
+    comm, n = input("Enter action on file: ").split()
     print('OK' if comm in file_permissions[n] else 'Access denied')
 
 print("End")
