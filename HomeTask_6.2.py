20b853e18d0bdc7c8f36385152cd88fe7cd32b09
edyankov <yankov.edik@gmail.com>
2022-12-02 17:23:25 +0200

    Init commit HomeTask_6.2

---
+++ HomeTask_6.2.py
@@ -0,0 +1,29 @@
+"""
+Задание #2:
+Права доступа
+Вирус повредил систему прав доступа к файлам. Известно, что над каждым файлом можно производить определенные действия:
+запись – W;
+чтение – R;
+запуск – X.
+На вход программе подается:
+число n – количество файлов;
+n строк с именами файлов и допустимыми операциями;
+число m – количество запросов к файлам;
+m запросов вида «операция файл».
+Для каждого допустимого запроса программа должна возвращать OK, для недопустимого – Access denied.
+"""
+
+file_permissions = {}
+access_permissions = {
+    'W': 'write',
+    'R': 'read',
+    'X': 'execute'
+}
+for i in range(int(input())):
+    x = input().split()
+    file_permissions[x[0]] = [access_permissions[i] for i in x[1:]]
+for i in range(int(input())):
+    comm, n = input().split()
+    print('OK' if comm in file_permissions[n] else 'Access denied')
+
+print("End")
