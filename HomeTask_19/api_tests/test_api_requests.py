4d61d5d536918268ac41cf223b4fa22c3a3bcc3e
edyankov <yankov.edik@gmail.com>
2023-02-26 18:33:21 +0200

    Init commit HomeTask_19

--- HomeTask_19/api_tests/test_api_requests.py
+++ HomeTask_19/api_tests/test_api_requests.py
@@ -5,7 +5,7 @@ import requests
 
 class TestApiReqres:
 
-    # @pytest.mark.api
+    @pytest.mark.api
     def test_get_users(self):
         url = 'https://reqres.in/api/users?page=2'
         response = requests.get(url)
@@ -13,14 +13,14 @@ class TestApiReqres:
         data = json.loads(response.text).get('data')
         print(len(data))
 
-    # @pytest.mark.api
+    @pytest.mark.api
     def test_get_user(self):
         url = 'https://reqres.in/api/users/9'
         response = requests.get(url)
         data = json.loads(response.text).get('data').get('id')
         assert data == 9
 
-    # @pytest.mark.api
+    @pytest.mark.api
     def test_authorization(self):
         url = 'https://reqres.in/api/login'
         body = {
@@ -31,7 +31,7 @@ class TestApiReqres:
         assert response.status_code == 200
         print(response.text)
 
-    # @pytest.mark.api
+    @pytest.mark.api
     def test_create_person(self):
         url = 'https://reqres.in/api/users'
         body = {
